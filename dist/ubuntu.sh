e83976f2e51038c5dbda1c47cfbbb73f7eb253b9
tuntunkun <naoya@tuntunkun.com>
2014-03-26 22:20:57 +0900

    modifyYXS

--- dist/ubuntu.sh
+++ dist/ubuntu.sh
@@ -8,17 +8,31 @@ sudo apt-get -y install python-pip
 sudo apt-get -y install python-dev
 
 #
+# install FUSE
+#
+sudo apt-get -y install fuse
+sudo apt-get -y install libfuse-dev
+
+#
 # install pip
 #
 easy_install pip
 
 #
-# virtualenv & activate
+# install virtualenv
 #
-pip install virtualenv
+sudo pip install virtualenv
+
+#
+# activate virtualenv
+#
 virtualenv .
 source ./bin/activate
 
+#
+# install virtualenv
+#
+pip install python-fuse
 
 # vim: set nu ts=2 autoindent : #
 
